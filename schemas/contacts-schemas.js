b125e8f8ee4621d9cc4ab83728932aa2840a40fe
RuslanTenderyts <rylya1995@gmail.com>
2023-05-20 22:31:18 +0300

    fix: add env-example

--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const contactAddSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
-    phone: Joi.string().regex(/^[0-9 ()-]{10,14}$/).required(),
+    phone: Joi.string().required(),
     favorite: Joi.boolean(),
 });
 
