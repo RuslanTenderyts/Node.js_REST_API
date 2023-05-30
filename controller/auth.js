b9f28fba0155881342d3d847f27ba489363ae198
RuslanTenderyts <rylya1995@gmail.com>
2023-05-30 11:59:04 +0300

    fix

--- controller/auth.js
+++ controller/auth.js
@@ -17,20 +17,22 @@ const register = async (req, res) => {
     const hashPassword = await bcrypt.hash(password, 10);
 
     const newUser = await User.create({...req.body, password: hashPassword});
-    res.status(201).json({        
+    res.status(201).json({ 
+        user: {       
           email: newUser.email,
           subscription: newUser.subscription,
+        }
     })
 };
 const login = async(req, res) => {
     const { email, password } = req.body;
         const user = await User.findOne({ email });
         if (!user) {
-            throw new HttpError(401, "Email or password invalid");
+            throw HttpError(401, "Email or password invalid");
         }
         const passwordCompare = await bcrypt.compare(password, user.password);
         if (!passwordCompare) {
-            throw new HttpError(401, "Email or password invalid");
+            throw HttpError(401, "Email or password invalid");
         }
 
         const payload = {
@@ -42,7 +44,7 @@ const login = async(req, res) => {
 
         res.json({
             token,
-            "user": {
+            user: {
                 email,
                 subscription: user.subscription,
             }
